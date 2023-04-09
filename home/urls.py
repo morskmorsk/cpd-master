f30b97876835d995180f8c24e7d6a07f9aaf97bd
morskmorsk <khalidfour@yahoo.com>
2023-04-08 20:03:49 -0600

    clean code up

--- home/urls.py
+++ home/urls.py
@@ -4,13 +4,12 @@ from .views import RegisterView, HomeView
 
 
 urlpatterns = [
-    # ... (other URL patterns)
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'),
-    #      name='login'),
-    path('', HomeView.as_view(), name='home'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('', HomeView.as_view(),
+         name='home'),
+    path('register/', RegisterView.as_view(),
+         name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html'),
          name='login'),
-    # ... (other URL patterns)
+    path('logout/', auth_views.LogoutView.as_view(),
+         name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
-# from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import TemplateView
-
-# Create your views here.
 
 
 class HomeView(TemplateView):
