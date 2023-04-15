b3e290cad65455213cd75fafd40bb12b890f75fa
morskmorsk <khalidfour@yahoo.com>
2023-04-15 01:10:29 -0600

    fix user create

---
+++ home/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('phoneNumber',)
--- home/views.py
+++ home/views.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm
+
+User = get_user_model()
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
 
-class RegisterView(generic.CreateView):
-    form_class = UserCreationForm
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'home/register.html'
 
